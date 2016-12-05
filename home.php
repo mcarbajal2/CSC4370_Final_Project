c52fc02c339f94d3fb3ed60a3765b4c8b0c12ee6
mcarbajal2 <mcarbajal2@student.gsu.edu>
2016-12-05 16:48:33 -0500

    updated css

---
+++ home.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<link href="finalproj.css" type="text/css"rel = "stylesheet"/>
+	<title>Home</title>
+</head>
+<body>
+	<div id = "home">
+		<h1 class = "welcome_bar">Welcome, !</h1><hr>
+	</div>
+	<div id = "home_body">
+		
+	</div>
+</body>
+</html>
