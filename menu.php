92f13011fab3686d3f65741ed067904bd771263f
mcarbajal2 <mcarbajal2@student.gsu.edu>
2016-12-05 17:14:13 -0500

    updated

---
+++ menu.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<link href="finalproj.css" type="text/css"rel = "stylesheet"/>
+	<title>Menu</title>
+</head>
+<body>
+	<div id = "menu">
+		<h1 class = "welcome_bar">Welcome, !</h1><hr>
+	</div>
+	<div id = "menu_body">
+		<p>Inventory
+		View Cart
+		Profile</p>
+	</div>
+</body>
+</html>
