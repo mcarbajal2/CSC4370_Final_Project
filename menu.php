8668e1dd77cbdd42df0200048fdfaf5335be48cb
mcarbajal2 <mcarbajal2@student.gsu.edu>
2016-12-05 18:07:46 -0500

    updated

--- menu.php
+++ menu.php
@@ -2,16 +2,17 @@
 <html>
 <head>
 	<link href="finalproj.css" type="text/css"rel = "stylesheet"/>
-	<title>Menu</title>
+	<title>Menu - ProwlShop.com</title>
 </head>
 <body>
 	<div id = "menu">
-		<h1 class = "welcome_bar">Welcome, !</h1><hr>
+		<h1 class = "welcome_bar">Welcome to the dark side!</h1>
+		<h4>prowlshop.com</h4><hr>
 	</div>
 	<div id = "menu_body">
-		<p>Inventory
-		View Cart
-		Profile</p>
+		<p>Prowel Shop Inventory</p>
+		<p>View Your Cart</p>
+		<p>Your Profile</p>
 	</div>
 </body>
 </html>
