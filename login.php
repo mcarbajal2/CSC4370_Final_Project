16d944271dac7934163f09e82d81f1817a120bb3
mcarbajal2 <mcarbajal2@student.gsu.edu>
2016-12-08 10:50:20 -0500

    12/8

--- login.php
+++ login.php
@@ -1,29 +1,36 @@
 <?
 include("http://codd.cs.gsu.edu/~mcarbajal2/final/config.php");
 
-$username=$_POST['name'];
-$password=$_POST['password'];
+$username = $_POST['name'];
+$password = $_POST['password'];
 
-if ($username == "" ||$password == "") {
-	//error alert
+if ($username == "" || $password == "") {
+	<script type="text/javascript">alert("You must enter a value for both fields.");</script>
 	<script type="text/javascript">location.href = 'login.html';</script>
 }
 
 if ($username == "admin") {
 	if ($password == "admin") {
-		//select admin data and load admin session
-		
 		//JavaScript to redirect to admin menu
 		<script type="text/javascript">location.href = 'admin.php';</script>
 	}
 }
 
 //SELECT to check if previous user, if so -> user menu
-
-//JavaScript to redirect
-<script type="text/javascript">location.href = 'menu.php';</script>
-
-//else "We don't recognize your username/password, please register again, thank you."
+$query = "SELECT Username FROM Users WHERE Username = "$username";"
+if ($query = null) {
+	<script type="text/javascript">alert("Username not recognized.");</script>
+	<script type="text/javascript">location.href = 'login.html';</script>
+}
+$query = "SELECT Password FROM Users WHERE Password = "$password";"
+else if ($query = null) {
+	<script type="text/javascript">alert("Password not recognized");</script>
+	<script type="text/javascript">location.href = 'login.html';</script>
+}
+else {
+	//JavaScript to redirect
+	<script type="text/javascript">location.href = 'menu.php';</script>
+}
 
 mysql_close();
 ?>
--- register.php
+++ register.php
@@ -1,53 +1,64 @@
 <?
 include("http://codd.cs.gsu.edu/~mcarbajal2/final/config.php");
 
-$username=$_POST['newUsername'];
-$password=$_POST['newPassword'];
-$email=$_POST['newEmail'];
-$fname=$_POST['newFname'];
-$lname=$_POST['newLname'];
+$username = $_POST['newUsername'];
+$password = $_POST['newPassword'];
+$email = $_POST['newEmail'];
+$fname = $_POST['newFname'];
+$lname = $_POST['newLname'];
 
-if ($username == "" ||$password == ""||$email == ""||$fname == ""||$lname == "") {
-	//error alert
+//checking for blank fields
+if ($username == "" || $password == ""|| $email == ""|| $fname == ""|| $lname == "") {
+	<script type="text/javascript">alert("You must enter a valid value for all fields.");</script>
 	<script type="text/javascript">location.href = 'register.html';</script>
 }
 
+//cannot register as admin
 if ($username == "admin") {
-	if ($password == "admin") {
-		//error, cannot use that login combination
-		<script type="text/javascript">location.href = 'register.html';</script>
-	}
+	<script type="text/javascript">alert("You cannot register using that username.");</script>
+	<script type="text/javascript">location.href = 'register.html';</script>
 }
 
 //check if previous user, if so -> username: $username / email: $email etc. already in system
+$query = "SELECT Fname FROM Users WHERE Email = "$email";";
+if ($query = null) {
+	<script type="text/javascript">alert("That email is already in use.");</script>
+	<script type="text/javascript">location.href = 'register.html';</script>
+}
 
+$query = "SELECT Fname FROM Users WHERE Username = "$username";";
+if ($query = null) {
+	<script type="text/javascript">alert("That username is already in use.");</script>
+	<script type="text/javascript">location.href = 'register.html';</script>
+}
 
 //Register user
-$query = "INSERT INTO Users VALUES ("'$username', '$password', '$newEmail', '$newFname', '$newLname'")";
+$query = "INSERT INTO Users VALUES ("'$newEmail', '$username', '$password', '$newFname', '$newLname'")";
 try {
 	mysql_query($query);
-	//Could insert user
+	//could register user
+	<script type="text/javascript">alert("You have been registered!");</script>
 }
 
 catch(Exception $e) {
-  //JavaScript alert "There was an error registering you, please try again. Thank you."
-  <script type="text/javascript">location.href = 'register.html';</script>
+	//could not register user
+	<script type="text/javascript">alert("There was an error registering you, please try again. Thank you.");</script>
+	<script type="text/javascript">location.href = 'register.html';</script>
 }
 
-//create user's purchase table
+//create user's orders table
 $query = "CREATE TABLE . $username . Orders(ID int(3) NOT NULL auto_increment, Product varchar(50) NOT NULL, Quantity int(20))";
 try {
 	mysql_query($query);
-	//Could insert user table
+	//Could create user table
 	<script type="text/javascript">location.href = 'menu.php';</script>
 }
 
 catch(Exception $e) {
-  //JavaScript alert "There was an error registering you, please try again. Thank you."
-  <script type="text/javascript">location.href = 'register.html';</script>
+	//could not create user table
+	<script type="text/javascript">alert("There was an error registering you, please try again. Thank you.");</script>
+	<script type="text/javascript">location.href = 'register.html';</script>
 }
-
-//JavaScript to redirect user
 
 mysql_close();
 ?>
