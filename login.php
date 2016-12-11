e5356008dd33eb765a6a7cebcfcf8887f926de52
mcarbajal2 <mcarbajal2@student.gsu.edu>
2016-12-11 13:07:48 -0500

    updated

--- login.php
+++ login.php
@@ -1,28 +1,50 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<link href="finalproj.css" type="text/css"rel = "stylesheet"/>
+	<title>Log in - ProwlShop.com</title>
+</head>
+<body>
+	<br /><br /><h1>Log In to Prowl Shop</h1><hr style = "width: 20%">
+	<div id = "login">
+	<form action = "" method = "POST">
+		<p>Username: <br /><br />
+		<input type = "text" name = "username"><br /><br />
+		Password: <br /><br />
+		<input type = "password" name = "password"><br /><br />
+		<a href = "register.php">New user?</a>
+		<input type = "submit" value = "Enter" name = "login">
+		</p>
+		</form>
+	</div>
+<body>
+</html>
+
 <?
 include("http://codd.cs.gsu.edu/~mcarbajal2/final/config.php");
 
-$username = $_POST['username'];
-$password = $_POST['password'];
+if(isset($_POST["login"])){
+	$username = $_POST['username'];
+	$password = $_POST['password'];
 
-if ($username == "admin" && $password == "admin") {
-	//go to admin menu
-	?><script type="text/javascript">location.href = 'admin.php';</script><?
-}
+	if ($username == "admin" && $password == "admin" && !empty($username) && !empty($password)) {
+		//go to admin menu
+		?><script type="text/javascript">location.href = 'admin.php';</script><?
+	}
 
-else {
-	//SELECT to check if previous user, if so -> user menu
-	$query = "SELECT Username FROM Users WHERE Username = "$username";"
-	if ($query != $username) {
-		?><script type="text/javascript">alert("Username not recognized.");</script>
-		<script type="text/javascript">location.href = 'login.html';</script><?
-	}
-	$query = "SELECT Password FROM Users WHERE Password = "$password";"
-	else if ($query != $password) {
-		?><script type="text/javascript">alert("Password not recognized");</script>
-		<script type="text/javascript">location.href = 'login.html';</script><?
-	}
 	else {
-		?><script type="text/javascript">location.href = 'menu.php';</script><?
+		//SELECT to check if previous user, if so -> user menu
+		$query = "SELECT Username FROM Users WHERE Username = '$username';"
+		if ($query != $username || empty($username)) {
+			?><script type="text/javascript">alert("Username not recognized.");</script><?
+		}
+		$query = "SELECT Password FROM Users WHERE Password = "$password";"
+		else if ($query != $password || !empty($password)) {
+			?><script type="text/javascript">alert("Password not recognized");</script><?
+		}
+		else {
+			?><script type="text/javascript">location.href = 'menu.php';</script><?
+		}
 	}
 }
 
