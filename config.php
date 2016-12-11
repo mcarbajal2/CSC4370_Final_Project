ddbdd664fd475e4e001431a7f7cfd023c93bcbfe
mcarbajal2 <mcarbajal2@student.gsu.edu>
2016-12-11 15:15:20 -0500

    updated

--- config.php
+++ config.php
@@ -1,7 +1,12 @@
+<?php
 $host = "localhost";
 $username = "mcarbajal2";
 $password = "mirnarypass";
 $database = "mcarbajal2";
 
-mysql_connect("$host", "$username", "$password")or die("cannot connect to server");
-mysql_select_db("$database")or die("cannot select database"); 
+$conn = new mysqli($host, $username, $password, $database);
+
+if($conn->connect_error) {
+	die("Connection failed: " . $conn->connect_error);
+}
+?>
