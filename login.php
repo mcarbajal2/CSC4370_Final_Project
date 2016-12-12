d518b675fd41c973524605a91f0628ea01e8ff9a
Jessica Luu <jluu4@student.gsu.edu>
2016-12-12 14:16:25 -0500

    12/12 changes

--- login.php
+++ login.php
@@ -1,53 +1,57 @@
 <!DOCTYPE html>
 <html>
-<head>
-	<link href="finalproj.css" type="text/css"rel = "stylesheet"/>
-	<title>Log in - ProwlShop.com</title>
-</head>
-<body>
-	<br /><br /><h1>Log In to Prowl Shop</h1><hr style = "width: 20%">
-	<div id = "login">
-	<form action = "" method = "POST">
-		<p>Username: <br /><br />
-		<input type = "text" name = "username"><br /><br />
-		Password: <br /><br />
-		<input type = "password" name = "password"><br /><br />
-		<a href = "register.php">New user?</a>
-		<input type = "submit" value = "Enter" name = "login">
-		</p>
-		</form>
-	</div>
-<body>
+    <head>
+        <link href="finalproj.css" type="text/css"rel = "stylesheet"/>
+        <title>Log in - ProwlShop.com</title>
+    </head>
+    <body>
+        <br /><br /><h1>Log In to Prowl Shop</h1><hr style = "width: 20%">
+        <div id = "login">
+            <form action = "" method = "POST">
+                <fieldset>
+                    <label><b>Username:</b></label><br><br>
+                    <input type = "text" name = "username" id="username"><br /><br />
+                    <label><b>Password:</b></label><br><br>
+                    <input type = "password" name = "password" id="password"><br /><br />
+                    <label><a href = "register.php">New user?</a></label>
+                    <input type="submit" value="Enter" name="login">
+                </fieldset>
+            </form>
+        </div>
+    </body>
 </html>
 
 <?php
 
 $dbHost = 'localhost';
-$dbUsername = 'vbarot1';
-$dbPassword = 'vbarot1';
-$dbName = 'vbarot1';
+$dbUsername = 'jluu4';
+$dbPassword = 'jluu4';
+$dbName = 'jluu4';
 
-$connect = mysqli_connect( $dbHost, $dbUsername, $dbPassword, $dbName);
-if($connect->connect_error) {
-        die("Connection failed: " . $connect->connect_error);
+$connect = mysql_connect($dbHost, $dbUsername, $dbPassword);
+$dbConnect = mysql_select_db($dbName);
+if (!$connect) {
+    die("Connection failed: " . mysql_error());
+}
+if (!$dbConnect) {
+    die("Database Connection failed: " . mysql_error());
 }
 
 if(isset($_POST["login"])){
 	$username = $_POST['username'];
 	$password = $_POST['password'];
-	$query = "SELECT * FROM Users WHERE username = '$username' AND password = '$password'";
-	$result = $connect->query($query);
-	if($result->num_rows > 0){		
-			header("menu.php");
-			session_start();
-			$row = $run->fetch_array(MYSQLI_ASSOC);
-			$_SESSION['ID'] = $row["ID"];
-			$_SESSION['User'] = $row["username"];
-			$_SESSION['Pass'] = $row["password"];
-			$_SESSION['fname'] = $row["fname"];
-			$_SESSION['lname'] = $row["lname"];
-			$_SESSION['email'] = $row["email"];
-	}
+    
+    $res = mysql_query("SELECT * FROM Users WHERE Username='$username' AND Password='$password'");
+    
+    $row = mysql_fetch_array($res);
+    
+    $count = mysql_num_rows($res);
+    
+    if ($count > 0) {
+        header("Location: menu.php");
+        session_start();
+    }
+    
 	else {
 		echo "<script>alert('Username and/or Password not recognized')</script>";
 		return;
